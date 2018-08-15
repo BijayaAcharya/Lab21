3ba03045251ffbf7dcd21b3efabf97ddb6e7ab1f
bijaya Acharya <bijaya2005@hotmail.com>
2018-08-15 17:01:47 -0400

    initial commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+
+
+        public ActionResult AddUser(UserInfo newUser) // creating new obj
+        {
+
+
+            if (ModelState.IsValid) //checks validation
+            {
+
+
+                ViewBag.ConfMessage = "Thanks" + newUser.FirstName;
+
+
+                return View("Confirm");
+            }
+
+            else
+            {
+                return View("Error"); // displays error message if the validation requirements are not met
+            }
+
+
+        }
+    }
+}
---
+++ Lab21/Models/UserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class UserInfo
+    {
+
+        // properties
+        // [Required]// 
+        //[RegularExpression("^[a-zA-Z]{2,30}$")]
+        public string FirstName { set; get; }
+
+        //[Required] // this field needs value
+        // [RegularExpression("^[a-zA-Z]{2,30}$")]
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Password { set; get; }
+
+        // default constructor
+        public UserInfo()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            PhoneNumber = "";
+            Password = "";
+
+
+        }
+        //constructors with parameters
+        public UserInfo(String fn, string ln, string em, string ph, string pass)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Email = em;
+            PhoneNumber = ph;
+            Password = pass;
+        }
+    }
+}
